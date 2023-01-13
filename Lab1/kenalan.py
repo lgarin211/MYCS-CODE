0da0f12331b1d7f1f6a475ef2fe5ff0570e2556d
Agustinus <57866141+lgarin211@users.noreply.github.com>
2023-01-13 06:15:30 +0000

    polimer

--- Lab1/kenalan.py
+++ Lab1/kenalan.py
@@ -1,7 +1,74 @@
-print("Hello")
+# print("Hello")
 
 name="agus"
 angka=20
 
-print(name)
-print(angka)
+# print(name)
+# print(angka)
+pat={
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[[  9  20  33  48]\n",
+      " [ 45  60  77  96]\n",
+      " [225 260 297 336]]\n"
+     ]
+    }
+   ],
+   "source": [
+    "array1 = np.array([[1, 2, 3, 4],[5, 6, 7, 8],[25, 26, 27, 28]])\n",
+    "array2 = np.array([[9,10,11,12]])\n",
+    "\n",
+    "# print(array1.shape)\n",
+    "# print(array1.shape)\n",
+    "# print(array2.shape)\n",
+    "print(array1*array2)\n",
+    "# print(np.dot(array1,array2))\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.4"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "3ad933181bd8a04b432d3370b9dc3b0662ad032c4dfaa4e4f1596c548f763858"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
+print(pat)
